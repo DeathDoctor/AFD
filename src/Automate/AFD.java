f2b0dbd67fcfc4f189ef6d4adf1950e17bda78eb
DeathDoctor <deathdoctor4499@gmail.com>
2015-12-30 00:46:32 +0100

    AFD

--- src/Automate/AFD.java
+++ src/Automate/AFD.java
@@ -7,11 +7,11 @@ public class AFD {
 	public static void main(String[] args) {
 		Scanner rep = new Scanner(System.in);
 		System.out.println("Veuillez saisir : ");
-		System.out.println("le nombre d'elements de votre alphabet :");
+		System.out.println("le nombre d'elements de votre alphabet : ");
 		int nbrLettres;
 		nbrLettres = rep.nextInt();
 		while(nbrLettres<=0){
-			System.out.println("Le nombre de lettre doit etre strictement positif ! Veuillez saisir à nouveau :");
+			System.out.println("Le nombre de lettre doit etre strictement positif ! Veuillez saisir à nouveau : ");
 			nbrLettres = rep.nextInt();
 		}
 		String monAlphabet[] = new String[nbrLettres];
@@ -19,7 +19,7 @@ public class AFD {
 			System.out.println("Entrez l'element n°"+(i+1)+" : ");
 			monAlphabet[i] = rep.next();
 		}
-		System.out.println("Le nombre d'etat de votre automate");
+		System.out.println("Le nombre d'etat de votre automate : ");
 		int nbrEtats;
 		nbrEtats = rep.nextInt();
 		while(nbrEtats<=0){
@@ -29,25 +29,24 @@ public class AFD {
 		System.out.println("Le nombre d'etats finals : ");
 		int nbrEtatsFinals;
 		nbrEtatsFinals = rep.nextInt();
-		while(nbrEtatsFinals<=0){
-			System.out.println("Le nombre d'etats finals doit etre strictement positif ! Veuillez saisir à nouveau :");
+		while((nbrEtatsFinals<=0)||(nbrEtatsFinals>nbrEtats)){
+			System.out.println("Le nombre d'etats finals doit etre strictement positif et inferieur au nombre d'états ! Veuillez saisir à nouveau : ");
 			nbrEtatsFinals = rep.nextInt();
 		}
 		Automate monAutomate = new Automate(monAlphabet,nbrEtats,nbrEtatsFinals);
 		System.out.println("Vous allez commencer à remplir la table de transition : ");
+		System.out.println("Veuillez respecter les caractérisques d'un AFD ! (Un seul etat intial ET avec une lettre on peut aller SEULEMENT vers UN etat) ");
 		System.out.println("Selon votre alphbet : ");
 		monAutomate.affichageAlphabet();
 		System.out.println("Remplissez-la introduisant les noms d'etats dont votre l'automate est construit ");
 		System.out.println("Pour aucune transition introduisez le mot 'NIL' ");
 		try {
 			monAutomate.tableDeTransion();
-		} catch (ArrayIndexOutOfBoundsException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-			System.out.println("YOOOOOOOOOOOOOOO  ");
-
+		    } catch (ArrayIndexOutOfBoundsException e) {
 		}
 		rep.close();
+		monAutomate.affichageTableTransition();
+		
 		}
 		
 		
--- src/Automate/Automate.java
+++ src/Automate/Automate.java
@@ -1,5 +1,6 @@
 package Automate;
 import java.util.Scanner;
+import java.util.*;
 public class Automate {
 	private String alphabet[]; // Un tableau de chaines caractères et non de caractère car une letrre peut se consistuer de plusieurs caractères
 	private int nbrEtats;      // Cette variable nous aide pour définir la taille de notre table de transition
@@ -13,42 +14,58 @@ public class Automate {
 		this.alphabet = alphabet;
 		this.nbrEtats = nbrEtats;
 		this.nbrEtatsFinals = nbrEtatsFinals;
-		this.tableTransiton = new String[this.nbrEtats+1][this.alphabet.length+1]; 
+		this.tableTransiton = new String[this.nbrEtats+1][this.alphabet.length+1]; // ( +1 ) Pour avoir une case supplémantaire 
 	}
 
 
 	void tableDeTransion(){            // La méthode qui permet de remplir la table de transition
 		this.tableTransiton[0][0] = ("Etats\\Alphabet");           // L'element au point (0,0) ne contient ni transition ni lettre 
-		for(int i=0;i<=this.alphabet.length;i++){                // La boucle permet de remplir la première ligne de la table avec les lettre de l'alphabet
+		for(int i=0;i<this.alphabet.length;i++){                // La boucle permet de remplir la première ligne de la table avec les lettre de l'alphabet
 			this.tableTransiton[0][i+1]= this.alphabet[i];
 		}
 		
 		// On a fini de remplir l'alphabet maintenant on remplit l'ensemble de nos etats dans la première colonne de la table 
 		
-		if(this.nbrEtats == 1){          // Cette condition est au cas où notre automate est constitué d'un seul état par conséquent cet état est qualifié oomme initial et final en meme temps
+		if(this.nbrEtats == 1){    // Cette condition est au cas où notre automate est constitué d'un seul état par conséquent cet état est qualifié oomme initial et final en meme temps
+			
 			this.tableTransiton[1][0] = (" --> * Eo");
 		}
-		else{               
-			System.out.println("Es-ce que votre etat initial est en meme temps un etat final ? Veuillez répondre par 'oui' ou 'non' : ");
+		else{  
+			
+			System.out.println("Es-ce que votre etat initial est en meme temps un etat final ? Veuillez répondre par : ");
+			System.out.println("1 - Pour 'oui' ");
+			System.out.println("2 - Pour 'non ");
 			Scanner rep = new Scanner(System.in);
-		    String reponse = rep.next();
-			while((reponse!="oui")||(reponse!="non")){
-				System.out.println("Réponse non identifiée ! Veuillez répondre par 'oui' ou 'non' : ");
-				reponse = rep.next();
+		    int reponse; 
+		    reponse = rep.nextInt();
+		    
+			while((reponse!=1)&&(reponse!=2)){
+				System.out.println("Réponse non identifiée ! Veuillez répondre par '1' ou '2' : ");
+				reponse = rep.nextInt();
 			}
+			
 			rep.close();
-			if(reponse.equals("non")){
-				this.tableTransiton[1][1] = ("--> Eo");
+			
+			if(reponse==2){
+				this.tableTransiton[1][1] = ("--> Eo ");
 			}
 			else{
-				this.tableTransiton[1][0] = (" --> * Eo");
+				this.tableTransiton[1][0] = ("--> * Eo ");
 			}
+			
+			int tmp = this.nbrEtats; // Cette variable nous permet d'avancer la fin notre première ligne pour remplir les états finals
+			
 			for(int i=this.nbrEtatsFinals;i>0;i--){ // La boucle permet de remplir les états finals de la table 
-				this.tableTransiton[0][i] = ("--> Ef"+String.valueOf(i));
+				this.tableTransiton[0][tmp] = ("* Ef"+String.valueOf(i));
+				tmp--;
 			}
+			
 			// Apres avoir rempli l'état initiale et le(s) état(s) final(s) on continue par remplir le reste des états s'ils existent
-			for(int i=2;i<(this.nbrEtats-this.nbrEtatsFinals);i++){ // On commence de 2 car le 1 conttient l'état initial et ( nbrEtats-nbrEtatsFinals ) pour ne pas déborder et écraser les états finals
+			
+			for(int i=2;i<=(this.nbrEtats-this.nbrEtatsFinals);i++){ // On commence de 2 car le 1 conttient l'état initial et ( nbrEtats-nbrEtatsFinals ) pour ne pas déborder et écraser les états finals
+				this.tableTransiton[0][i] = ("E"+String.valueOf(i-1));
 			}
+			
 		}
 		Scanner transition = new Scanner(System.in);
 		for(int i=1;i<this.alphabet.length;i++){
@@ -57,7 +74,7 @@ public class Automate {
 				System.out.println("Dans quel etats de fera la trasition avec la lettre : "+this.tableTransiton[0][j]);
 				String etat;
 				etat = transition.next();
-				while(etatExiste(this.tableTransiton,etat)==false){ // Vérification ! (la méthode est déclaré un peu plus bas)
+				while((etatExiste(this.tableTransiton,etat))==false){ // Vérification ! (la méthode est déclaré un peu plus bas)
 					System.out.println("L'etat saisi n'existe pas dans votre automate ! Veuillez saisir de nouveau : ");
 					etat = transition.next();
 				}
@@ -68,23 +85,73 @@ public class Automate {
 		
 	}
 	
+	void affichageTableTransition(){         // La méthode qui permet d'afficher la table de transition 
+		
+		System.out.println("Voici la table de transition de votre AFD : ");
+		for(int i=0;i<this.tableTransiton.length;i++){
+			for(int j=0;j<this.tableTransiton[i].length;j++){
+				System.out.print("| "+this.tableTransiton[i][j]+" ");
+			}
+			System.out.println("____________________________________________");
+		}
+	}
+	
 	boolean etatExiste(String[][] tableDeTransition, String etat){ // La méthode permet vérifier si l'état existent parmis les états dont l'automate est consititué
 		int i=1;
-		while((tableDeTransition[0][i]!=etat)&&(i<=this.nbrEtats)){
+		while(!(tableDeTransition[0][i].equals(etat))&&(i<=this.nbrEtats)){
 			i++;
 		}
-		if(i<=this.nbrEtats){
+		i = this.nbrEtats;
+		if((i<=this.nbrEtats)){  // On compare maintenant à E0 et au(x) Ef(s) car dans la table de transition ils sont stockés comme : " --> Eo, * Ef1 , ...;" 
+			return true;
+		}
+		else{
+			if(etat.equals("E0")){
+				return true;
+			}
+			else{
+				i=1;
+				String etatFinal="Ef"+String.valueOf(i);
+				while((i<=this.nbrEtatsFinals)&&(!(etat.equals(etatFinal)))){
+					i++;
+					etatFinal="Ef"+String.valueOf(i);
+				}
+				if(i<=this.nbrEtatsFinals){
+					return true;
+				}
+				else{
+					return false;
+				}
+				
+				
+			}
+			
+		}
+	}
+	
+	boolean lettreExiste(String lettre){  // La méthode qui permet de voir si une lettre existe dans notre alphabet
+		int i=0;
+		while(!(this.alphabet[i].equals(lettre))&&(i<=this.alphabet.length)){
+			i++;
+		}
+		if(i<=this.alphabet.length){
 			return true;
 		}
 		else{
 			return false;
 		}
+		
 	}
 	
     void affichageAlphabet(){
     	System.out.print("{");
     	for(int i=0;i<this.alphabet.length;i++){
-			System.out.print(this.alphabet[i]+", ");
+    		if(i==(this.alphabet.length - 1)){
+    			System.out.print(this.alphabet[i]);
+    		}
+    		else{
+    			System.out.print(this.alphabet[i]+",");
+    		}
 		}
 		System.out.println("}");
     }
